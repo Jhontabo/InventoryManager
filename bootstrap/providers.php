62e6fca6a0aa94916766cb83eee11d5139f86271
hacker <jhonse.tajumbina@gmail.com>
2026-03-07 22:10:53 -0500

    fix: register auth service provider for gate super-admin bypass

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,6 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\AuthServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\Filament\DocentePanelProvider::class,
     App\Providers\Filament\EstudiantePanelProvider::class,
